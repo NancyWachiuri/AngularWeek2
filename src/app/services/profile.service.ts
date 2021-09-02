b3fb1b66352f7108541ead5ce8cdc476a7416362
NancyWachiuri <nancywachiuri@gmail.com>
2021-09-02 09:35:28 +0300

    create the services directory

---
+++ src/app/services/profile.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+//import 'rxjs/add/operator/map';
+//import { resolve } from 'path';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+
+
+  private username:string;
+  private clientid = ' Iv1.83df423d2d4f3c38';
+  private clientsecret = 'qgAdbgWbSE23d9tlpWf3KH75/jZmP+X6NTXNcmwu7w4=';
+  public users_data:any
+
+  constructor(private http:HttpClient) { 
+    console.log("service is now ready!");
+    this.username = 'Nancy';
+  }
+
+  getProfileInfo(){
+    return this.http.get("https://api.github.com/users/" + this.username + "?cient_id=" + this.clientid + " client_secret=" + this.clientsecret);
+    
+  }
+
+  getProfileRepos(){
+
+    return this.http.get("https://api.github.com/users/" + this.username + "/repos?cient_id=" + this.clientid + " client_secret=" + this.clientsecret);
+    
+  }
+
+  updateProfile(username:string){
+    this.username = username
+  }
+}
